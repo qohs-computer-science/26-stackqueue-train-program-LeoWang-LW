e0cd06d1552a115cad6e04feb1d47e5fc4182dd5
LeoWang-LW <yuzhewang2017@gmail.com>
2026-01-07 17:47:42 +0000

    finished train class 1/7/26

--- HelloWorldProject/src/MyProgram.java
+++ HelloWorldProject/src/MyProgram.java
@@ -1,8 +1,8 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * TODO: Leo Wang
+ * TODO: 01/7/25
+ * TODO: 6
+ * TODO: Program to sort trains by mileage, destination, and weight to different tracks. In order to ensure trains can reach destination safely. 
  */
 import java.util.Scanner;
 import java.io.File;
@@ -14,18 +14,32 @@ public class MyProgram {
 	public static int val = 0;
 	public static void main(String[] args) {
 
+		Queue <Train> O = new LinkedList<Train>(); 
+		Queue <Train> needMaintenance = new LinkedList<Train>(); 
+		Queue <Train> A = new LinkedList<Train>(); 
+		Queue <Train> B = new LinkedList<Train>(); 
+		Queue <Train> C = new LinkedList<Train>(); 
+		Queue <Train> other = new LinkedList<Train>(); 
+
+		int weightA, weightB, weightC; 
+
 		int limitTrackA = 100000, limitTrackB = 100000, limitTrackC = 100000;
 	
 		Scanner x = new Scanner(System.in);
 		try{
 			File f = new File("HelloWorldProject/src/data.txt");
 			x = new Scanner (f);
-			String name = x.nextLine();
-			System.out.println(name);
+			//String name = x.nextLine();
+			//System.out.println(name);
+			int count; 
+			while(count )
 		}
 		catch(Exception e)
 		{
 			System.out.println(e.getMessage());
 		}
+
+		
+
 	}
 }
--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -2,5 +2,37 @@ public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
 
+    public String name(){
+        return name;
+    }
 
+    public String contents(){
+        return product; 
+    }
+
+    public String to(){
+        return destination; 
+    }
+
+    public String from(){
+        return origin; 
+    }
+
+    public int getWeight(){
+        return weight; 
+    }
+
+    public int getMileage(){
+        return miles; 
+    }
+
+    public Train(String n, String p, String o, String d, int w, int m){
+        this.name = n; 
+        this.product = p; 
+        this.origin = o; 
+        this.destination = d; 
+        this.weight = w; 
+        this.miles = m; 
+    }
 }
+
